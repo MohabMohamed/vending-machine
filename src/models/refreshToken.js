813f607fbbc92188c715101ac6b0cf446e257162
Mohab Abd El-Dayem <mohab.m.mohamed@gmail.com>
2022-03-27 13:58:47 +0200

    Add RefreshToken model

---
+++ src/models/refreshToken.js
@@ -0,0 +1,47 @@
+const db = require('../db')
+const { DataTypes } = require('sequelize')
+const { wrongToken } = require('../errors/tokenError')
+
+const RefreshToken = db.sequelize.define(
+    'RefreshToken', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        token: {
+            type: DataTypes.STRING(400),
+            allowNull: false
+        }
+    }, {
+        tableName: 'RefreshToken'
+    }
+)
+
+RefreshToken.getUserByToken = async refreshToken => {
+    const matchedRefreshToken = await RefreshToken.findOne({
+        where: {
+            token: refreshToken
+        },
+        include: 'user'
+    })
+    if (!matchedRefreshToken) {
+        throw wrongToken()
+    }
+    return { user: matchedRefreshToken.user, matchedRefreshToken }
+}
+
+RefreshToken.associate = models => {
+    RefreshToken.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user',
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+    })
+}
+
+module.exports = RefreshToken
--- src/models/user.js
+++ src/models/user.js
@@ -5,93 +5,98 @@ const UserDto = require('../dtos/user')
 const bcrypt = require('bcryptjs')
 
 const User = db.sequelize.define(
-  'User',
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    username: {
-      type: DataTypes.STRING(20),
-      unique: true,
-      allowNull: false,
-      validate: {
-        isLowercase: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    deposit: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-      allowNull: false,
-      validate: {
-        isNotNegative (value) {
-          if (value < 0) throw new Error("deposit can't be a negative number")
+    'User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING(20),
+            unique: true,
+            allowNull: false,
+            validate: {
+                isLowercase: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        deposit: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+            validate: {
+                isNotNegative(value) {
+                    if (value < 0) throw new Error("deposit can't be a negative number")
+                }
+            }
+        },
+        role: {
+            type: DataTypes.ENUM('buyer', 'seller')
         }
-      }
-    },
-    role: {
-      type: DataTypes.ENUM('buyer', 'seller')
-    }
-  },
-  {
-    tableName: 'User',
-    hooks: {
-      beforeSave: async (user, options) => {
-        user.username = user.username.toLowerCase()
-        user.password = await bcrypt.hash(user.password, 10)
-      },
-      beforeUpdate: async (user, options) => {
-        user.username = user.username.toLowerCase()
-        const { _previousDataValues, dataValues } = user
-        if (
-          await !bcrypt.compare(
-            dataValues.password,
-            _previousDataValues.password
-          )
-        ) {
-          user.password = await bcrypt.hash(user.password, 10)
+    }, {
+        tableName: 'User',
+        hooks: {
+            beforeSave: async(user, options) => {
+                user.username = user.username.toLowerCase()
+                user.password = await bcrypt.hash(user.password, 10)
+            },
+            beforeUpdate: async(user, options) => {
+                user.username = user.username.toLowerCase()
+                const { _previousDataValues, dataValues } = user
+                if (
+                    await !bcrypt.compare(
+                        dataValues.password,
+                        _previousDataValues.password
+                    )
+                ) {
+                    user.password = await bcrypt.hash(user.password, 10)
+                }
+            }
         }
-      }
     }
-  }
 )
 
-User.findByCredentials = async (username, password) => {
-  const user = await User.findOne({
-    where: {
-      username
+User.findByCredentials = async(username, password) => {
+    const user = await User.findOne({
+        where: {
+            username
+        }
+    })
+
+    if (!user) {
+        throw userError.unableToLogin()
     }
-  })
 
-  if (!user) {
-    throw userError.unableToLogin()
-  }
+    const isMatch = await bcrypt.compare(password, user.password)
 
-  const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        throw userError.unableToLogin()
+    }
 
-  if (!isMatch) {
-    throw userError.unableToLogin()
-  }
-
-  return user
+    return user
 }
 
-User.prototype.toJSON = function () {
-  return new UserDto(user)
+User.prototype.toJSON = function() {
+    return new UserDto(user)
 }
 
 User.associate = models => {
-  User.hasMany(models.Product, {
-    foreignKey: 'sellerId',
-    as: 'product',
-    onUpdate: 'CASCADE',
-    onDelete: 'CASCADE'
-  })
+    User.hasMany(models.Product, {
+        foreignKey: 'sellerId',
+        as: 'product',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    })
+
+    User.hasMany(models.RefreshToken, {
+        foreignKey: 'userId',
+        as: 'refreshToken',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+    })
 }
 
 module.exports = User
